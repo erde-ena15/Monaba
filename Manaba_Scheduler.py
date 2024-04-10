c021079f16b6638f6c179a5a2e18a3827c407995
erde-ena15 <erde.ena15@gmail.com>
2024-04-11 01:21:17 +0900

    ログイン情報のローカル保存の実装

--- Manaba_Scheduler.py
+++ Manaba_Scheduler.py
@@ -1,15 +1,66 @@
-﻿import requests
+﻿#from optparse import Option
+#from tkinter import Y
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
 import json
 from getpass import getpass
 from IPython.display import display
+import tkinter.filedialog
+#import tkinter, tkinter.filedialog, tkinter.messagebox
 #from google.colab import drive
 
-
+#Load = 0でロード, =1でセーブ
+def login_info_manager(Save,Load,User,Pass):
+  match Save:
+      case "1":
+          pass
+      case "2":
+          match Load:
+              case 0:
+                  fTyp = [("","login_info.json")]
+                  iDir = os.path.expanduser('~')
+                  file_path = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
+                  if os.path.exists(file_path) :
+                    with open(file_path, mode ='r') as f :
+                        login1 = json.load(f)
+                        USER = login1['USER']
+                        PASS = login1['PASS']
+                        return USER,PASS
+                  else :
+                    print('ファイルが存在しない、もしくは見つかりません')
+              case 1:               
+                  login0 = {'USER': User, 'PASS': Pass}
+                  path = folder + '/login_info.json'
+                  with open(path, mode='w') as f:
+                    json.dump(login0, f, ensure_ascii=False)
+      case "3":  
+          pass
+  
+  
 
 def scraping_manaba():
+ 
+   
+  '''  
+  root = tkinter.Tk()
+  root.withdraw()
+  fTyp = [("","*")]
+  iDir = os.path.abspath(os.path.dirname(__file__))
+  tkinter.messagebox.showinfo('○×プログラム','処理ファイルを選択してください！')
+  file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+  
+
+
+
+  # 処理ファイル名の出力
+  tkinter.messagebox.showinfo('○×プログラム',file)
+  '''
+  
+  
+
+
 
   #もし、ログイン情報があれば読み込む
   '''mount_path = '/content/drive'
@@ -26,8 +77,13 @@ def scraping_manaba():
         USER = login1['USER']
         PASS = login1['PASS']
   else :'''
-  USER = input('ユーザーIDを入力してください: ')
-  PASS = getpass('パスワードを入力してください: ')
+  login = login_info_manager(save,0,None,None)
+  if not login == None:
+    USER = login[0]
+    PASS = login[1]
+  else:
+    USER = input('ユーザーIDを入力してください: ')
+    PASS = getpass('パスワードを入力してください: ')
 
   url_login = "https://cit.manaba.jp/ct/login"
   session = requests.session()
@@ -65,10 +121,8 @@ def scraping_manaba():
     return
   else :
     #ログイン情報を保存
-    login0 = {'USER': USER, 'PASS': PASS}
-    with open('login_info.json', mode='w') as f:
-      json.dump(login0, f, ensure_ascii=False)
-
+    login_info_manager(save,1,USER,PASS)
+    
     #各要素取得
     types = soup.select('td[class="center"]') #部分一致のため
     titles = soup.find_all('div', class_='myassignments-title')
@@ -110,14 +164,18 @@ def scraping_manaba():
     return data
 
 #以下実行部
-print("動作を指定してください")
-print('------動作モード------')
-print("1.表示のみ")
-print("2.新規csvを作成")
-print("3.既存csvを更新")
-print("4.差分csvを作成")
-print('----------------------')
+save = "1"
+string = "なし"
+
 while True:
+    print("----------Manaba-Scheduler ver.1.0----------")
+    print('------動作モード------')
+    print("1.表示のみ")
+    print("2.新規csvを作成")
+    print("3.既存csvを更新")
+    print("4.差分csvを作成")
+    print("5.オプション")
+    print('----------------------')
     mode = input("動作モードの番号を入力してください: ")
     match mode:
         case "1":
@@ -125,6 +183,9 @@ while True:
             if not data == None :
               df = pd.DataFrame(data)
               display(df)
+            Continue = input("終了しますか?(y/n): ")
+            if Continue == "n":
+              continue
             break
         case "2":
             data = scraping_manaba()
@@ -132,6 +193,9 @@ while True:
               df = pd.DataFrame(data)
               filename ='未提出課題.csv'
               df.to_csv(filename, index = False)
+            Continue = input("終了しますか?(y/n); ")
+            if Continue == "n":
+              continue
             break
         case "3":
             data = scraping_manaba()
@@ -157,6 +221,9 @@ while True:
                 df_old.to_csv(filename, index = False)
               else :
                 print('過去のファイルが存在しない、もしくは見つかりません')
+            Continue = input("終了しますか?(y/n): ")
+            if Continue == "n":
+              continue
             break
         case "4":
             data = scraping_manaba()
@@ -202,6 +269,38 @@ while True:
                   print("差分は見つかりませんでした。")
               else :
                 print('過去のファイルが存在しない、もしくは見つかりません')
+            Continue = input("終了しますか?(y/n)")
+            if Continue == "n":
+              continue    
             break
+            
+        case "5":
+            
+                    
+            print("----オプション一覧----")
+            print("1.ログイン情報の保存: %s" % (string))
+            print("2.戻る")
+            print('----------------------')
+            option = input("変更したいオプションの番号を入力してください: ")
+            match option:
+                case "1":
+                    save = input("1.なし/2.端末に保存/3.googleドライブに保存: ")
+                    match save:
+                         case "1":
+                             string = "なし"
+                         case "2":                           
+                             iDir = os.path.expanduser('~')
+                             folder = tkinter.filedialog.askdirectory(initialdir=iDir)
+                             string = "端末に保存 保存先:" + folder
+                             
+                         case "3":
+                             string = "googleドライブに保存"
+                         case _:
+                             print('存在しないオプションです')
+                case "2":
+                    pass
+                case _:
+                    print('存在しないオプションです')
+
         case _:
-             print('存在しないオプションです')
+            print('存在しないオプションです')
