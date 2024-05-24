9585430db62ac11e95198a6ac3df4e50bd245669
erde-ena15 <erde.ena15@gmail.com>
2024-05-24 22:35:01 +0900

    v.1.2.7_リリース版

--- source/main.py
+++ source/main.py
@@ -9,7 +9,7 @@ def main(page: ft.Page):
     time.sleep(0.4)
     #global
     page.title = "Monaba"
-    version = "1.2.6"
+    version = "1.2.7"
     USER = ft.TextField(label="ユーザーID")
     PASS = ft.TextField(label="パスワード", password=True, can_reveal_password=True)
     ERROR = ft.Text("",color='RED')
