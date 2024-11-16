2c3cfd27b1baa144eb652fba7a3df998b546e47d
erde-ena15 <erde.ena15@gmail.com>
2024-11-16 17:42:14 +0900

    v1.2.9
    バグ修正

--- source/main.py
+++ source/main.py
@@ -3,6 +3,7 @@ import module
 import sys
 import time
 import datetime
+import pytz
 #import pandas as pd 
 
 def main(page: ft.Page):   
@@ -16,17 +17,20 @@ def main(page: ft.Page):
     ERROR = ft.Text("",color='RED')
     SETTING = {'USER':None,'PASS':None,'MOBILE':None,'ISSAVE':False}
     temp ={'USER':None,'PASS':None}
+    last_update_time = None
 
     if page.platform_brightness.name == "DARK":
             Color = ft.colors.with_opacity(0.05, "#007000")
             Bgcolor = ft.colors.with_opacity(1.00, "#001800")
             N1color = ft.colors.with_opacity(1.00, "#6AE860")
             N2color = ft.colors.with_opacity(1.00, "#002300")
+            strcolor = ft.colors.with_opacity(1.00, "#FFFFFF")
     else:
         Color = ft.colors.GREEN_50
         Bgcolor = ft.colors.GREEN_100
         N1color="green_accent_700",
         N2color = ft.colors.GREEN_100
+        strcolor = ft.colors.with_opacity(1.00, "#000000")
 
     dlg_title=ft.Text("")
     dlg_modal = ft.AlertDialog(
@@ -45,7 +49,7 @@ def main(page: ft.Page):
         load_setting()
         page.views.append(create_setting())
         page.update()
-
+        
     def load_setting():
         nonlocal SETTING
         SETTING['USER'] = page.client_storage.get("SaveUser")    
@@ -66,8 +70,8 @@ def main(page: ft.Page):
 
     Navi=ft.NavigationBar(
         destinations=[
-            ft.NavigationDestination(icon=ft.icons.EXPLORE_OUTLINED,selected_icon=ft.icons.EXPLORE,label="task"),
-            ft.NavigationDestination(icon=ft.icons.SETTINGS_OUTLINED,selected_icon=ft.icons.SETTINGS,label="setting"),
+            ft.NavigationBarDestination(icon=ft.icons.EXPLORE_OUTLINED,selected_icon=ft.icons.EXPLORE,label="task"),
+            ft.NavigationBarDestination(icon=ft.icons.SETTINGS_OUTLINED,selected_icon=ft.icons.SETTINGS,label="setting"),
                     ],
         bgcolor=Bgcolor,
         on_change=on_change_navi
@@ -82,26 +86,20 @@ def main(page: ft.Page):
         page.client_storage.set("Mobile",SETTING['MOBILE'])
         #再読み込み対策
         if not page.route == "/":
+            e.control.disabled == True
+            page.update()
             reload()
+            e.control.disabled == False
+            page.update()
+        else:
+            time.sleep(0.2)
+            page.views.clear()
+            load_setting()
+            page.views.append(create_home())
+            page.update()
 
     CHECKBOX = ft.Checkbox(label="   モバイル表示", value=SETTING['MOBILE'],on_change=mobile_change)
-    
-    def auto_login_change(e):
-        nonlocal SETTING
-        if auto_login_switch.value == True and temp["USER"]:
-            SETTING['ISSAVE'] = True
-            page.client_storage.set("SaveUser",temp["USER"]) 
-            page.client_storage.set("SavePass",temp["PASS"])  
-        else:
-            SETTING['ISSAVE'] = False
-            page.client_storage.remove("SaveUser")
-            page.client_storage.remove("SavePass")
-        page.client_storage.set("IsSave",SETTING['ISSAVE'])
-        #再読み込み対策
-        reload()
-       
-    auto_login_switch = ft.Switch(label="自動ログイン", value=SETTING['ISSAVE'],on_change=auto_login_change)
-    
+
     def create_home():      
         nonlocal SETTING
         layout = [
@@ -129,7 +127,7 @@ def main(page: ft.Page):
  #ft.Row([],alignment=ft.MainAxisAlignment.CENTER),
   
     def login_clicked(e):            
-        page.dialog = dlg_modal
+        page.overlay.append(dlg_modal)
         dlg_modal.open = True
         nonlocal SETTING 
         page.update()
@@ -138,7 +136,7 @@ def main(page: ft.Page):
            PASS.value = SETTING["PASS"]
         nonlocal Userdata
         Userdata = module.manaba.manaba_tool(USER.value,PASS.value)
-        print(id(Userdata))
+        #print(id(Userdata))
         result = Userdata.login_manaba()
         if result == -1: 
             dlg_modal.open = False
@@ -166,11 +164,18 @@ def main(page: ft.Page):
             dlg_modal.open = False
             page.update()
             page.go("/task")
+    
+    def on_keyboard(e: ft.KeyboardEvent):
+        if e.key == "Enter":
+            login_clicked(e)
+
+    page.on_keyboard_event = on_keyboard
 
     def create_task():      
-        dt_now = datetime.datetime.now()
+        dt_now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
         str = dt_now.strftime('この内容は%Y年%m月%d日 %H:%M:%Sに取得された情報です\n内容が古い場合は右の更新ボタンを押してください')
-        
+        update_snack_bar = ft.SnackBar(ft.Text("時間を置いてお試しください",color = strcolor),bgcolor = N2color)
+
         if page.platform_brightness.name == "DARK":
             Color = ft.colors.with_opacity(0.05, "#007000")
             Bgcolor = ft.colors.with_opacity(1.00, "#001800")
@@ -178,18 +183,43 @@ def main(page: ft.Page):
             Color = ft.colors.GREEN_50
             Bgcolor = ft.colors.GREEN_100
         Navi.bgcolor = Bgcolor
-        
+
         def button_clicked(e):
-            page.views.clear()
-            PAGE=create_task()
-            page.views.append(PAGE)
-            page.update()
+            total_seconds = 0
+            nonlocal last_update_time
+            now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+            if last_update_time == None:
+                page.views.clear()
+                PAGE=create_task()
+                page.views.append(PAGE)
+                page.update()
+                last_update_time = now
+                return
+            elif now.date() != last_update_time.date():
+                total_seconds = 86400
+            total_seconds = total_seconds + now.hour * 3600 + now.minute * 60 + now.second
+            last_total_seconds = last_update_time.hour * 3600 + last_update_time.minute * 60 + last_update_time.second
+            if total_seconds - last_total_seconds < 5:
+                update_snack_bar.open = True
+                page.update()
+                return
+            else:
+                try:
+                    if busy == True:
+                        return #連打対策
+                except NameError:
+                    busy = True
+                    page.views.clear()
+                    PAGE=create_task()
+                    page.views.append(PAGE)
+                    page.update()
+                    last_update_time = now
         
-        b = ft.IconButton(icon=ft.icons.AUTORENEW, on_click=button_clicked,)
-        
+        update_button = ft.IconButton(icon=ft.icons.AUTORENEW, on_click=button_clicked)
+
         def get_detail(e):
             dlg_title.value = "loading"
-            page.dialog = dlg_modal
+            page.overlay.append(dlg_modal)
             dlg_modal.open = True
             search = e.control.data
             nonlocal detail
@@ -217,10 +247,10 @@ def main(page: ft.Page):
             if X == None:
                 layout = [ 
                         ft.AppBar(title = ft.Text("未提出課題"), bgcolor=Bgcolor),ft.Text(""),ft.Text(""),
-                        ft.Container(ft.Row([ft.Text(str,size=11,width=280),b],spacing=0,alignment=ft.MainAxisAlignment.CENTER),margin=8),
+                        ft.Container(ft.Row([ft.Text(str,size=11,width=280),update_button],spacing=0,alignment=ft.MainAxisAlignment.CENTER),margin=8),
                         ft.Row([ft.Container(content=ft.Text("未提出課題はありません( ´∀｀)b",width=250),margin=15,alignment=ft.alignment.center,
                                              padding=15,bgcolor=Color,border_radius=10,width=250)],alignment=ft.MainAxisAlignment.CENTER),
-                                             Navi]
+                                             update_snack_bar,Navi]
             else:
                 for D in range(len(X[0])):
                     tit = ft.DataCell(ft.Text(f"{X[1][D]}",size=11,weight="BOLD"),on_tap=get_detail,data=D)
@@ -232,7 +262,7 @@ def main(page: ft.Page):
                     ROWS.append(ft.DataRow(cells = CELLS))
                 layout = [                    
                         ft.AppBar(title = ft.Text("未提出課題"), bgcolor=Bgcolor),    
-                        ft.Container(ft.Row([ft.Text(str,size=11,width=280),b],spacing=0),margin=8,),
+                        ft.Container(ft.Row([ft.Text(str,size=11,width=280),update_button],spacing=0),margin=8,),
                         ft.Container(
                             ft.DataTable(
                                         border=ft.border.all(2, "green"),
@@ -250,16 +280,17 @@ def main(page: ft.Page):
                                         ),
                         margin=10,
                         ),
+                        update_snack_bar,
                         Navi,                
                     ]
         else:
             if X == None:
                 layout = [ 
                         ft.AppBar(title = ft.Text("未提出課題"), bgcolor=Bgcolor),ft.Text(""),ft.Text(""),
-                        ft.Container(ft.Row([ft.Text(str,width=385),b],alignment=ft.MainAxisAlignment.CENTER),margin=10),
+                        ft.Container(ft.Row([ft.Text(str,width=385),update_button],alignment=ft.MainAxisAlignment.CENTER),margin=10),
                         ft.Row([ft.Container(content=ft.Text("未提出課題はありません( ´∀｀)b",width=250),margin=15,alignment=ft.alignment.center,
                                              padding=15,bgcolor=Color,border_radius=10,width=250)],alignment=ft.MainAxisAlignment.CENTER),
-                                             Navi]
+                                             update_snack_bar,Navi]
             else:
                 for D in range(len(X[0])):
                     tit = ft.DataCell(ft.Text(f"{X[1][D]}"),on_tap=get_detail,data=D)
@@ -274,7 +305,7 @@ def main(page: ft.Page):
 
                 layout = [                    
                             ft.AppBar(title = ft.Text("未提出課題"), bgcolor=Bgcolor),                       
-                            ft.Container(ft.Row([ft.Text(str),b],alignment=ft.MainAxisAlignment.CENTER),margin=10,),
+                            ft.Container(ft.Row([ft.Text(str),update_button],alignment=ft.MainAxisAlignment.CENTER),margin=10,),
                             ft.Row([
                                         ft.DataTable(
                                         border=ft.border.all(2, "green"),
@@ -291,7 +322,7 @@ def main(page: ft.Page):
                                         rows = ROWS,
                                         )
                                     ],alignment=ft.MainAxisAlignment.CENTER),                          
-                            Navi,               
+                            update_snack_bar,Navi,               
                         ]
         return ft.View("/task",layout,scroll = "HIDDEN") 
 
@@ -299,14 +330,34 @@ def main(page: ft.Page):
         return ft.View("/loading")
     
     def create_setting():      
+        nonlocal SETTING
+        def auto_login_change(e):
+            if auto_login_switch.value == True and temp["USER"]:
+                SETTING['ISSAVE'] = True
+                page.client_storage.set("SaveUser",temp["USER"]) 
+                page.client_storage.set("SavePass",temp["PASS"])  
+            else:
+                SETTING['ISSAVE'] = False
+                page.client_storage.remove("SaveUser")
+                page.client_storage.remove("SavePass")
+            page.client_storage.set("IsSave",SETTING['ISSAVE'])
+            #再読み込み対策
+            e.control.disabled == True
+            page.update()
+            reload()
+            e.control.disabled == False
+            page.update()
+        
+        auto_login_switch = ft.Switch(label="自動ログイン", value=SETTING['ISSAVE'],on_change=auto_login_change)
+        
         if page.platform_brightness.name == "DARK":
             Bgcolor = ft.colors.with_opacity(1.00, "#001800")
         else:
             Bgcolor = ft.colors.GREEN_100
         Navi.bgcolor = Bgcolor
-        log = "v1.2.8 : 取得時間確認機能と更新ボタン追加\nv1.2.0 : 詳細内容表示機能追加\nv1.1.0 : 自動ログイン追加\nv1.0.0 : 初期ビルド"
-        ab = f"ver.{version}\nplatform: {sys.platform}\ncontact:erde.ena15@gmail.com"
-        nonlocal SETTING
+        log = "v1.2.9 : バグを修正\nv1.2.8 : 取得時間確認機能と更新ボタン追加\nv1.2.0 : 詳細内容表示機能追加\nv1.1.0 : 自動ログイン追加\nv1.0.0 : 初期ビルド"
+        ab = f"ver.{version}\nplatform: {page.platform.value}\ncontact:erde.ena15@gmail.com"
+        
         layout = [
                     ft.AppBar(title = ft.Text("設定"), bgcolor=Bgcolor), 
                     ft.Text(""),
@@ -397,7 +448,7 @@ def main(page: ft.Page):
             page.views.append(create_home())      
         if page.route == "/loading": #ダイアログが表示されない場合
              page.views.append(create_loading()) #pageがない場合必要かも？(自動ログイン時等)
-             page.dialog = dlg_modal
+             page.overlay.append(dlg_modal)
              dlg_modal.open = True
         if page.route == "/task":
             if not Userdata:
